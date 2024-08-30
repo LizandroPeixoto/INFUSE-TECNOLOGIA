61bfcf061c5d395e7e7c4ad373b654ea7e343e71
LizandroPeixoto <49523995+LizandroPeixoto@users.noreply.github.com>
2024-08-30 17:50:49 -0300

    AJustes finais

--- INFUSE/src/test/java/com/br/INFUSE/controler/PedidoControllerTests.java
+++ INFUSE/src/test/java/com/br/INFUSE/controler/PedidoControllerTests.java
@@ -4,6 +4,7 @@ package com.br.INFUSE.controler;
 import com.br.INFUSE.model.Pedido;
 import com.br.INFUSE.repositer.PedidoRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
